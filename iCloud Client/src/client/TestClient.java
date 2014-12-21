4ee7e9ab294f15a3a45b3ff2c593a23cd724c1c6
robotman3000 <robotman3000@icloud.com>
2014-12-21 08:49:56 -0500

    Now will run a temporary test method for getting user contacts; this is to test if the login/logout process works

--- iCloud Client/src/client/TestClient.java
+++ iCloud Client/src/client/TestClient.java
@@ -1,45 +1,33 @@
 package client;
 
 import icloud.UserSessionInstance;
-//import icloud.contacts.ContactManager;
-//import icloud.contacts.AddressBook;
-//import icloud.contacts.ContactManager;
+import icloud.contacts.ContactManager;
 
 public class TestClient {
 
 	public static void main(String[] args) throws Exception {
-		boolean debug = true;
+		boolean debugEnabled = true;
 
 		String username = args[0];
 		String password = args[1];
-		if (debug) {
+		
+		if (debugEnabled) {
 			System.out.println("Username: " + username + "\n" + "Password: " + password);
 		}
 
 		UserSessionInstance user1 = new UserSessionInstance(username, password, false);
 		user1.connect();
 		System.out.println("Connected");
-		user1.disconnect();
-		System.out.println("Disconnected");
+		//ContactManager user1Contacts = user1.getContactManager();
+		user1.abc123();
 		
-		if (debug) {
+
+		if (debugEnabled) {
 			System.out.println("The current users ID is: " + user1.getUserID());
 		}
-
-		
-		//ContactManager user1ContactsManager = new ContactManager(user1);
-		
-		/*
-		 * System.out.println("Configuring Contacts..."); //ContactManager
-		 * contacts = new ContactManager(user1);
-		 * 
-		 * System.out.println("Getting All Address Books and Contacts...");
-		 * //AddressBook[] addrBooks = contacts.getAddressBooks();
-		 * 
-		 * //addrBooks = contacts.getAddressBooks();
-		 * //contacts.getContactsList(addrBooks[0]);
-		 */
-
+		user1.disconnect();
+		System.out.println("Disconnected");
+		user1.abc123();
 	}
 
 }
