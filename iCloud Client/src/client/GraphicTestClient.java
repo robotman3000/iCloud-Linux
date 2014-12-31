1a7be7ed3713b49335a44d3708fc17cacef7972d
robotman3000 <robotman3000@icloud.com>
2014-12-31 17:16:02 -0500

    Added a place holder class for the gui

---
+++ iCloud Client/src/client/GraphicTestClient.java
@@ -0,0 +1,43 @@
+package client;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class GraphicTestClient {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GraphicTestClient window = new GraphicTestClient();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GraphicTestClient() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
