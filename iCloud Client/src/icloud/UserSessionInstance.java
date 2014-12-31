47e6bd1248ce9470acc7a7abf943d9ea8b1d3133
robotman3000 <robotman3000@icloud.com>
2014-12-31 17:14:38 -0500

    Added more methods that were missing

--- iCloud Client/src/icloud/UserSessionInstance.java
+++ iCloud Client/src/icloud/UserSessionInstance.java
@@ -3,199 +3,70 @@ package icloud;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import icloud.calendars.CalendarManager;
-import icloud.contacts.ContactManager;
-import icloud.mail.MailManager;
-import icloud.notes.NoteManager;
-import icloud.reminders.ReminderManager;
-
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.net.CookieHandler;
-import java.net.CookieManager;
-import java.net.CookiePolicy;
-import java.net.CookieStore;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.HttpCookie;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class UserSessionInstance {
 	private String username;
 	private String password;
 	private boolean extended_login;
 
-	private String userID;
-	private String UUID;
-	private static final String clientBnum = "14H40";
-	private List<HttpCookie> cookies;
-	
-	private CalendarManager calendars;
-	private ContactManager contacts;
-	private MailManager mail;
-	private NoteManager notes;
-	private ReminderManager reminders;
+	private Map<String, Map<String, String>> serversList = new HashMap<String, Map<String, String>>();
+	private Map<String, String> userProperties = new HashMap<String, String>();
 
-	public UserSessionInstance(String username, String password, boolean extended_login) {
+	public final String UUID;
+	public final String clientBnum = "14H40";
+
+	private UserTokenManager userTokens;
+	private boolean debugenabled;
+	private boolean announceConnections;
+
+	public UserSessionInstance(String username, String password, boolean extended_login, boolean debugenabled, boolean announceConnections) {
 		this.username = username;
 		this.password = password;
 		this.extended_login = extended_login;
+		this.debugenabled = debugenabled;
+		this.announceConnections = announceConnections;
+		this.UUID = CommonLogic.generateUUID();
 	}
 
-	// DO NOT USE ########################
-	public void connect(boolean test) throws Exception {
-		boolean debugenabled = true;
-
-		// Generate Auth String
-		String authString = "{\"apple_id\":" + "\"" + username + "\"" + ",\"password\":" + "\"" + password + "\"" + ",\"extended_login\":" + extended_login + "}";
-
-		// Debug Output
-		if (debugenabled) {
-			System.out.println("Authentication String: " + authString);
-			System.out.println("UUID: " + (UUID = CommonLogic.generateUUID()));
-			UUID = UUID.toUpperCase();
-			CommonLogic.splitOut();
-		}
-
-		System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
-
-		// Create the cookie manager
-		CookieManager manager = new CookieManager();
-		manager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
-		CookieHandler.setDefault(manager);
-
-		URL httpurl = new URL("https://setup.icloud.com:443/setup/ws/1/login?" + "clientBuildNumber=" + clientBnum + "&" + "clientId=" + UUID);
-		HttpURLConnection httpconnection = (HttpURLConnection) httpurl.openConnection();
-		// httpurl = new URL("https://setup.icloud.com:443/setup/ws/1/login");
-
-		// Debug Output
-		if (debugenabled) {
-			System.out.println("URL is: " + httpurl);
-			System.out.println("URL Details: {");
-			System.out.println("Protocol: " + httpurl.getProtocol());
-			System.out.println("Host: " + httpurl.getHost());
-			System.out.println("Port: " + httpurl.getPort());
-			System.out.println("Path: " + httpurl.getPath());
-			System.out.println("File: " + httpurl.getFile());
-			System.out.println("Query: " + httpurl.getQuery());
-			System.out.println("}");
-			CommonLogic.splitOut();
-		}
-
-		// Set Headers
-		// TODO: Remove Magic Values
-		httpconnection.setRequestMethod("POST");
-		httpconnection.setRequestProperty("Host", "setup.icloud.com");
-		httpconnection.setRequestProperty("User-Agent", "Mozilla/5.0 Java_iCloud/1.0 LoginManager/1.0");
-		httpconnection.setRequestProperty("Accept", "text/json");
-		httpconnection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-		// httpconnection.setRequestProperty("Accept-Encoding", "deflate");
-		httpconnection.setRequestProperty("Referer", "https://www.icloud.com/");
-		httpconnection.setRequestProperty("Content-Type", "text/json; charset=UTF-8");
-		httpconnection.setRequestProperty("Origin", "https://www.icloud.com"); // //
-		httpconnection.setRequestProperty("Connection", "keep-alive");
-		httpconnection.setRequestProperty("Cache-Control", "no-cache"); // HTTP
-		httpconnection.setRequestProperty("Pragma", "no-cache"); // HTTP 1.0.
-
-		if (debugenabled) {
-			System.out.println(httpconnection.getRequestMethod());
-			System.out.println(httpconnection.getRequestProperties());
-			CommonLogic.splitOut();
-		}
-
-		httpconnection.setDoInput(true);
-		httpconnection.setDoOutput(true);
-		httpconnection.setUseCaches(false);
-
-		// Send Post Data
-		// Change to datastreamwriter
-		DataOutputStream dos = new DataOutputStream(httpconnection.getOutputStream());
-		dos.writeBytes(authString);
-		dos.flush();
-		dos.close();
-
-		if (debugenabled) {
-			System.out.println("Input: " + httpconnection.getDoInput() + "\n" + "Output: " + httpconnection.getDoOutput());
-			System.out.println("URL: " + httpconnection.getURL() + "\n" + "Response Message: " + httpconnection.getResponseMessage() + "\n" + "Returned Headers: " + httpconnection.getHeaderFields());
-			//System.out.println("Error Stream: " + convertStreamToString(httpconnection.getErrorStream()));
-			CommonLogic.splitOut();
-
-		}
-
-		// Read Input
-		InputStream is = httpconnection.getInputStream();
-		InputStreamReader isr = new InputStreamReader(is);
-
-		int numCharsRead;
-		char[] charArray = new char[1024];
-		StringBuffer sb = new StringBuffer();
-		while ((numCharsRead = isr.read(charArray)) > 0) {
-			sb.append(charArray, 0, numCharsRead);
-		}
-		String result = sb.toString();
-
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		JsonParser jp = new JsonParser();
-		JsonElement je = jp.parse(result);
-		String result2 = gson.toJson(je);
-
-		if (debugenabled) {
-			System.out.println("*** BEGIN ***");
-			System.out.println(result2);
-			System.out.println("*** END ***");
-		}
-
-		CookieStore cookieJar = manager.getCookieStore();
-		List<HttpCookie> cookies = cookieJar.getCookies();
-
-		if (debugenabled) {
-			for (HttpCookie cookie : cookies) {
-				System.out.println("CookieHandler retrieved cookie: " + cookie);
-			}
-		}
-
-	}
-	// ###################################
-	
 	public void connect() throws Exception {
+		
+		if (announceConnections){
+			System.out.println("Connecting to: Login Server");
+		}
+		// TODO: Make all magic values be generated or read from a config file
 
 		Map<String, String> headersMap = new HashMap<String, String>();
 
 		String authString = "{\"apple_id\":" + "\"" + username + "\"" + ",\"password\":" + "\"" + password + "\"" + ",\"extended_login\":" + extended_login + "}";
-		boolean debugenabled = true;
 		// Debug Output
 		if (debugenabled) {
 			System.out.println("Authentication String: " + authString);
-			System.out.println("UUID: " + (UUID = CommonLogic.generateUUID()));
-			UUID = UUID.toUpperCase();
+			System.out.println("UUID: " + UUID);
 			CommonLogic.splitOut();
 		}
 
 		URL httpUrl = new URL("https://setup.icloud.com:443/setup/ws/1/login?" + "clientBuildNumber=" + clientBnum + "&" + "clientId=" + UUID);
 
 		headersMap.put("Origin", "https://www.icloud.com");
+		headersMap.put("User-Agent", "Mozilla/5.0 Java_iCloud/1.0 LoginManager/1.0");
 
-		ServerConnection conn = new ServerConnection(true);
+		ServerConnection conn = new ServerConnection(debugenabled);
 		conn.setServerUrl(httpUrl);
 		conn.setRequestMethod("POST");
 		conn.setRequestHeaders(headersMap);
 		conn.setPayload(authString);
 		conn.connect();
 
+		parseUserProps(conn.getResponseData());
+
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		JsonParser jp = new JsonParser();
 		JsonElement je = jp.parse(conn.getResponseData());
@@ -207,49 +78,46 @@ public class UserSessionInstance {
 			System.out.println("*** END ***");
 		}
 
-		cookies = conn.getResponseCookies();
-
-		if (debugenabled) {
-			for (HttpCookie cookie : cookies) {
-				System.out.println("CookieHandler retrieved cookie: " + cookie);
-			}
-		}
+		userTokens = new UserTokenManager(UUID, conn.getResponseCookies());
 
 	}
 
 	public void disconnect() throws Exception {
-		boolean debugenabled = true;
-		
+		if (announceConnections){
+			System.out.println("Connecting to: Logout Server");
+		}
+		// TODO: Make all magic values be generated or read from a config file
+
 		Map<String, String> headersMap = new HashMap<String, String>();
 		String valCookie = "";
 		String var = "X-APPLE-WEBAUTH-VALIDATE";
 
-		Iterator<HttpCookie> iterator = cookies.iterator();
+		Iterator<HttpCookie> iterator = userTokens.getUserTokens().iterator();
 		while (iterator.hasNext()) {
 			HttpCookie key = iterator.next();
 			String name = key.getName();
-			String value = key.getValue();
-			//headersMap.put("Cookie: " + name, value);
 			int number = name.compareTo(var);
 			if (number == 0) {
 				valCookie = key.getValue();
-				//System.out.println(valCookie);
+				// System.out.println(valCookie);
 
 			}
 		}
 
 		headersMap.put("Origin", "https://www.icloud.com");
-		
-		ServerConnection conn = new ServerConnection(true);
+		headersMap.put("User-Agent", "Mozilla/5.0 Java_iCloud/1.0 LoginManager/1.0");
 
-		URL httpUrl = new URL("https://setup.icloud.com:443/setup/ws/1/logout?" + "clientBuildNumber=" + clientBnum + "&clientId=" + UUID + "&token=" + valCookie + "&dsid=" + "8084583249" + "&proxyDest=" + "p30-setup");
+		ServerConnection conn = new ServerConnection(debugenabled);
+
+		Map<String, String> accountServer = serversList.get("account");
+		URL httpUrl = new URL(accountServer.get("url") + "/setup/ws/1/logout?" + "clientBuildNumber=" + clientBnum + "&clientId=" + UUID + "&token=" + valCookie + "&dsid=" + getUserID() + "&proxyDest=" + "p30-setup");
 		conn.setServerUrl(httpUrl);
 		conn.setRequestMethod("POST");
 		conn.setRequestHeaders(headersMap);
-		conn.setRequestCookies(cookies);
+		conn.setRequestCookies(userTokens.getUserTokens());
 		conn.setPayload("{}");
 		conn.connect();
-		
+
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		JsonParser jp = new JsonParser();
 		JsonElement je = jp.parse(conn.getResponseData());
@@ -260,58 +128,113 @@ public class UserSessionInstance {
 			System.out.println(result2);
 			System.out.println("*** END ***");
 		}
-		
+
 	}
 
-	//public Map<> getUserProps() {
+	private void parseUserProps(String string) {
+		JsonParser jp = new JsonParser();
+		JsonElement je = jp.parse(string);
 
-	//	return null;
-	//}
+		JsonObject userData = je.getAsJsonObject();
+		JsonElement webServices = userData.get("webservices");
+		JsonObject webServices2 = webServices.getAsJsonObject();
 
-	public String getUserID() {
-		return userID;
+		String[] webServiceStrings = { "reminders", "mail", "drivews", "settings", "keyvalue", "push", "contacts", "findme", "photos", "ubiquity", "iwmb", "ckdatabasews", "docws", "account", "streams", "notes", "calendar" };
+		String[] memberStrings = { "status", "url", "pcsRequired" };
+		for (String str : webServiceStrings) {
+			JsonElement obj = webServices2.get(str);
+			JsonObject jelement = obj.getAsJsonObject();
+			Map<String, String> map1 = new HashMap<String, String>();
+			for (String mstr : memberStrings) {
+				if (jelement.has(mstr)) {
+					JsonElement json = jelement.get(mstr);
+					map1.put(mstr, json.getAsString());
+				}
+			}
+			serversList.put(str, map1);
+		}
+
+		JsonElement jselement = userData.get("dsInfo");
+		JsonObject dsInfo = jselement.getAsJsonObject();
+		String[] dsInfoStrings = { "lastName", "appleId", "dsid", "languageCode", "fullName", "firstName" };
+
+		for (String str : dsInfoStrings) {
+			if (dsInfo.has(str)) {
+				JsonElement var = dsInfo.get(str);
+				userProperties.put(str, var.getAsString());
+
+			}
+		}
+
 	}
 
-	public Map<String, BaseManager> getAllManagers() {
-
-		return null;
+	public UserTokenManager getTokenManager() {
+		return userTokens;
 	}
 
-	public ContactManager getContactManager() {
-		initContactManger();
-		return contacts;
-	}
-
-	private void initContactManger() {
-		
+	public String getServerUrl(String url){
+		if (serversList.containsKey(url)){
+			return serversList.get(url).get("url");
+		}
+		return CommonLogic.LOCALHOST;
 	}
 	
-	public void abc123() throws Exception{
+	public String getUserID() {
+		return userProperties.get("dsid");
+	}
+
+	public String getAppleID() {
+		return userProperties.get("appleId");
+	}
+
+	public String getLocale() {
+		return userProperties.get("languageCode");
+	}
+
+	public String getFirstName() {
+		return userProperties.get("firstName");
+	}
+
+	public String getLastName() {
+		return userProperties.get("lastName");
+	}
+
+	public String getFullName() {
+		return userProperties.get("fullName");
+	}
+
+	@SuppressWarnings("unused")
+	private void abc123() throws Exception {
 		URL httpUrl = new URL("https://p30-contactsws.icloud.com/co/startup?" + "clientBuildNumber=" + clientBnum + "&" + "clientId=" + UUID + "&dsid=" + "8084583249" + "&locale=" + "en_US" + "&order=" + "last,first" + "&clientVersion" + "2.1");
 
 		Map<String, String> headersMap = new HashMap<String, String>();
 		headersMap.put("Origin", "https://www.icloud.com");
 
-		ServerConnection conn = new ServerConnection(true);
+		ServerConnection conn = new ServerConnection(debugenabled);
 		conn.setServerUrl(httpUrl);
 		conn.setRequestMethod("GET");
 		conn.setRequestHeaders(headersMap);
-		conn.setRequestCookies(cookies);
+		conn.setRequestCookies(userTokens.getUserTokens());
 		conn.connect();
-		
+
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		JsonParser jp = new JsonParser();
 		JsonElement je = jp.parse(conn.getResponseData());
 		String result2 = gson.toJson(je);
 
-		boolean debugenabled = true;
-		if (debugenabled ) {
+		if (debugenabled) {
 			System.out.println("*** BEGIN ***");
 			System.out.println(result2);
 			System.out.println("*** END ***");
 		}
-		
+
+		userTokens.updateUserTokens(conn.getResponseCookies(), UUID);
+
+		if (debugenabled) {
+			for (HttpCookie cookie : userTokens.getUserTokens()) {
+				System.out.println("CookieHandler retrieved cookie: " + cookie);
+			}
+		}
 	}
 
-	
 }
