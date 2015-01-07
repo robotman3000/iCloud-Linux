08d9e2b37dcb69904c8d4c97931266de6bbe1dc3
robotman3000 <robotman3000@icloud.com>
2015-01-06 19:52:34 -0500

    Added a debug gui

--- iCloud Client/src/client/GraphicTestClient.java
+++ iCloud Client/src/client/GraphicTestClient.java
@@ -1,27 +1,37 @@
 package client;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class GraphicTestClient {
+public class GraphicTestClient extends TestClient{
 
 	private JFrame frame;
+	private String username;
+	private String password;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GraphicTestClient window = new GraphicTestClient();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		GraphicTestClient window = new GraphicTestClient();
+		window.frame.setVisible(true);
+		
+		boolean debugEnabled = true;
+		boolean announceConnections = true;
+
+		if (args.length != 2) {
+			System.exit(1);
+		}
+		
+		window.username = args[0];
+		window.password = args[1];
+
+		
+		
 	}
 
 	/**
@@ -38,6 +48,135 @@ public class GraphicTestClient {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+
+		JButton connectButton = new JButton();
+		connectButton.setSize(120, 30);
+		connectButton.setLocation(10, 10);
+		connectButton.setText("Connect");
+		connectButton.setToolTipText("Opens Connection To Server");
+
+		connectButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					openConnection(username, password, false, true, true);
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		frame.add(connectButton);
+
+		JButton disconnectButton = new JButton();
+		disconnectButton.setSize(120, 30);
+		disconnectButton.setLocation(10, 50);
+		disconnectButton.setText("Disconnect");
+		disconnectButton.setToolTipText("Closes Connection To Server");
+
+		disconnectButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					closeConnection();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		frame.add(disconnectButton);
+		
+		JButton printUserPropsButton = new JButton();
+		printUserPropsButton.setSize(120, 30);
+		printUserPropsButton.setLocation(10, 90);
+		printUserPropsButton.setText("Print Properties");
+		printUserPropsButton.setToolTipText("Gets User Properties");
+
+		printUserPropsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				printUserProps();
+			}
+		});
+		
+		frame.add(printUserPropsButton);
+		
+		JButton getNoteManButton = new JButton();
+		getNoteManButton.setSize(120, 30);
+		getNoteManButton.setLocation(140, 10);
+		getNoteManButton.setText("Create Note Manager");
+		getNoteManButton.setToolTipText("Gets User Note Manager");
+
+		getNoteManButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					createNoteManager();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		frame.add(getNoteManButton);
+		
+		JButton printNotesButton = new JButton();
+		printNotesButton.setSize(120, 30);
+		printNotesButton.setLocation(140, 50);
+		printNotesButton.setText("Print Notes");
+		printNotesButton.setToolTipText("Prints User Notes");
+
+		printNotesButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					printNotes(noteMgr);
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		frame.add(printNotesButton);
+		
+		JButton deleteNoteButton = new JButton();
+		deleteNoteButton.setSize(120, 30);
+		deleteNoteButton.setLocation(140, 90);
+		deleteNoteButton.setText("Delete Note");
+		deleteNoteButton.setToolTipText("Deletes a Note");
+
+		deleteNoteButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					deleteNote();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		frame.add(deleteNoteButton);
+		
+		JButton noteChangesetButton = new JButton();
+		noteChangesetButton.setSize(120, 30);
+		noteChangesetButton.setLocation(140, 130);
+		noteChangesetButton.setText("Note Changeset");
+		noteChangesetButton.setToolTipText("");
+
+		noteChangesetButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					noteMgr.getChanges();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		frame.add(noteChangesetButton);
 	}
 
 }
--- iCloud Client/src/client/TestClient.java
+++ iCloud Client/src/client/TestClient.java
@@ -15,13 +15,19 @@ import icloud.notes.NoteManager;
 
 public class TestClient {
 
+	UserSessionInstance user1;
+	UserTokenManager user1Tokens;
+	NoteManager noteMgr;
+	protected boolean connected = false;
+	protected boolean hasNoteMan = false;
+
 	public static void main(String[] args) throws Exception {
-		
+
 		TestClient var = new TestClient();
-		
+
 		boolean debugEnabled = true;
 		boolean announceConnections = true;
-		
+
 		if (args.length != 2) {
 			System.exit(1);
 		}
@@ -29,80 +35,141 @@ public class TestClient {
 		String password = args[1];
 
 		if (debugEnabled) {
-			System.out.println("Username: " + username + "\n" + "Password: " + password);
+			System.out.println("Username: " + username + "\n" + "Password: "
+					+ password);
 		}
 
-		UserSessionInstance user1 = new UserSessionInstance(username, password, false, debugEnabled, announceConnections);
-		user1.connect();
-		System.out.println("Connected");
-		
 		if (debugEnabled) {
-			System.out.println("The current users ID is: " + user1.getUserID());
-			System.out.println("The current users Apple ID is: " + user1.getAppleID());
-			System.out.println("The current users First Name is: " + user1.getFirstName());
-			System.out.println("The current users Last Name is: " + user1.getLastName());
-			System.out.println("The current users Full Name is: " + user1.getFullName());
-			System.out.println("The current users Locale is: " + user1.getLocale());
+			var.printUserProps();
 		}
-		
-		UserTokenManager user1Tokens = user1.getTokenManager();
-		
-		NoteManager user1Notes = new NoteManager(user1, user1Tokens, debugEnabled);
-		
-		var.printNotes(user1Notes);
-		
-		//Note newNote = new Note("2014-12-31T10:11:01-05:00", CommonLogic.generateUUID(), "/", "18", "abc123", "test content");
-		//user1Notes.createNote(newNote, newNote.getNoteID());
-		
-		Scanner reader = new Scanner(System.in);
-		System.out.println("Enter the Note to delete");
-		//get user input for a
-		String a = reader.next();
-		System.out.println(a);
-		reader.close();
-		
-		user1Notes.deleteNote(a);
-		
-		var.printNotes(user1Notes);
-		
-		user1.disconnect();
-		System.out.println("Disconnected");
-		
+
+		// NoteManager user1Notes = new NoteManager(user1, user1Tokens,
+		// debugEnabled);
+
+		// var.printNotes(user1Notes);
+
+		// Note newNote = new Note("2014-12-31T10:11:01-05:00",
+		// CommonLogic.generateUUID(), "/", "18", "abc123", "test content");
+		// user1Notes.createNote(newNote, newNote.getNoteID());
+
+		/*
+		 * Scanner reader = new Scanner(System.in);
+		 * System.out.println("Enter the Note to delete"); // get user input for
+		 * a String a = reader.next(); System.out.println(a); reader.close();
+		 * 
+		 * user1Notes.deleteNote(a);
+		 * 
+		 * var.printNotes(user1Notes);
+		 */
+
 	}
 
-	private void printNotes(NoteManager user1Notes) {
-		// TODO Auto-generated method stub
-		Set<String> notebookList = user1Notes.getNotebookList();
-		Iterator<String> iterator = notebookList.iterator();
-		
-		CommonLogic.splitOut();
-		System.out.println("Begin Note Echo");
-		CommonLogic.splitOut();
-		while (iterator.hasNext()){
-			String nextNotebook = iterator.next();
-			Set<String> notesList = user1Notes.getNotesList(nextNotebook);
-			Iterator<String> iter = notesList.iterator();
-			
-			System.out.println("Note Manager \"toString\"" + "\n" + user1Notes.toString());
-			
-			while(iter.hasNext()){
-
-				String nextNote = iter.next();
-				Note tempNote = user1Notes.getNote(nextNote, nextNotebook);
-				
-				Document noteContent = Jsoup.parse(tempNote.getContent());
-				
-				System.out.println("Current Note ID: " + tempNote.getNoteID());
-				System.out.println("Current Note Folder: " + tempNote.getFolder());
-				System.out.println("Current Note Date Modified: " + tempNote.getDateModified());
-				System.out.println("Current Note Size: " + tempNote.getSize());
-				System.out.println("Current Note Subject: " + tempNote.getSubject());
-				System.out.println("Current Note Content: " + noteContent.toString());
-				
-			}
+	protected void openConnection(String username, String password,
+			boolean extendedLogin, boolean debugEnabled,
+			boolean announceConnections) throws Exception {
+		if (connected) {
+			return;
 		}
-		CommonLogic.splitOut();
-		System.out.println("End Note Echo");
-		CommonLogic.splitOut();
+		user1 = new UserSessionInstance(username, password, extendedLogin,
+				debugEnabled, announceConnections);
+		user1.connect();
+		System.out.println("Connected");
+		connected = true;
+		user1Tokens = user1.getTokenManager();
+	}
+
+	protected void closeConnection() throws Exception {
+		if (connected != true) {
+			return;
+		}
+		user1.disconnect();
+		System.out.println("Disconnected");
+		connected = false;
+		hasNoteMan = false;
+		user1 = null;
+		user1Tokens = null;
+	}
+
+	protected void printUserProps() {
+		if (connected != true) {
+			return;
+		}
+		System.out.println("The current users ID is: " + user1.getUserID());
+		System.out.println("The current users Apple ID is: "
+				+ user1.getAppleID());
+		System.out.println("The current users First Name is: "
+				+ user1.getFirstName());
+		System.out.println("The current users Last Name is: "
+				+ user1.getLastName());
+		System.out.println("The current users Full Name is: "
+				+ user1.getFullName());
+		System.out.println("The current users Locale is: " + user1.getLocale());
+	}
+
+	protected void createNoteManager() throws Exception {
+		if (connected != true) {
+			return;
+		}
+		noteMgr = new NoteManager(user1, user1Tokens, true);
+		hasNoteMan = true;
+
+	}
+
+	protected void deleteNote() throws Exception {
+		if (connected == true && hasNoteMan == true) {
+			Scanner reader = new Scanner(System.in);
+			System.out.println("Enter the Note to delete");
+			// get user input for a
+			String a = reader.next();
+			System.out.println(a);
+			reader.close();
+
+			noteMgr.deleteNote(a);
+		}
+	}
+
+	protected void printNotes(NoteManager user1Notes) {
+		if (connected == true && hasNoteMan == true) {
+			// TODO Auto-generated method stub
+			Set<String> notebookList = user1Notes.getNotebookList();
+			Iterator<String> iterator = notebookList.iterator();
+
+			CommonLogic.splitOut();
+			System.out.println("Begin Note Echo");
+			CommonLogic.splitOut();
+			while (iterator.hasNext()) {
+				String nextNotebook = iterator.next();
+				Set<String> notesList = user1Notes.getNotesList(nextNotebook);
+				Iterator<String> iter = notesList.iterator();
+
+				System.out.println("Note Manager \"toString\"" + "\n"
+						+ user1Notes.toString());
+
+				while (iter.hasNext()) {
+
+					String nextNote = iter.next();
+					Note tempNote = user1Notes.getNote(nextNote, nextNotebook);
+
+					Document noteContent = Jsoup.parse(tempNote.getContent());
+
+					System.out.println("Current Note ID: "
+							+ tempNote.getNoteID());
+					System.out.println("Current Note Folder: "
+							+ tempNote.getFolderName());
+					System.out.println("Current Note Date Modified: "
+							+ tempNote.getDateModified());
+					System.out.println("Current Note Size: "
+							+ tempNote.getSize());
+					System.out.println("Current Note Subject: "
+							+ tempNote.getSubject());
+					System.out.println("Current Note Content: "
+							+ noteContent.toString());
+
+				}
+			}
+			CommonLogic.splitOut();
+			System.out.println("End Note Echo");
+			CommonLogic.splitOut();
+		}
 	}
 }
